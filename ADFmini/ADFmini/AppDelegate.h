6cb59ec025f89113983ab35010ae22633be1a7f7
CYC666 <13705038428@163.com>
2018-11-23 20:39:39 +0800

    no message

---
+++ ADFmini/ADFmini/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADFmini
+//
+//  Created by 曹老师 on 2018/11/23.
+//  Copyright © 2018 曹奕程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADFmini/ADFmini/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADFmini
+//
+//  Created by 曹老师 on 2018/11/23.
+//  Copyright © 2018 曹奕程. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
